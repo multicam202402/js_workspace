08806e3a203c737ae4b9cffc03ac80e52cbb5046
multicampus202402 <multicam202402@gmail.com>
2024-02-08 13:37:23 +0900

    금일수업

--- js/lib.js
+++ js/lib.js
@@ -5,3 +5,30 @@ function dan(n){
         document.write(`${n}*${i}=${n*i} <br>`);
     }
 } 
+
+/*----------------------------------------------
+랜덤값 반환하기    
+max 값까지의 랜덤한 값 구하기 단,
+getRandom(5);   0~5 까지
+----------------------------------------------*/
+function getRandom(max){
+    let n = (max+1)*Math.random(); //0.0xxxxx  1보다 작은 실수가 난수로 반환됨
+    //700.3489348540923
+    n = parseInt(n);
+    //console.log(n);
+    return n; //결과를 리턴
+}
+
+/*----------------------------------------------
+최소값과 최대값을 지정하여 반환되는 랜덤
+getRandomByRange(2, 6)
+----------------------------------------------*/
+function getRandomByRange(min, max){
+    let result = min + getRandom(max-min);
+    //console.log(result);
+    return result;
+}
+
+
+
+
