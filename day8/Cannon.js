8a2448aabbf46542dea1cb3a7cfd28d90eb96e20
multicampus202402 <multicam202402@gmail.com>
2024-02-20 11:58:02 +0900

    금일수업

--- day8/Cannon.js
+++ day8/Cannon.js
@@ -39,6 +39,11 @@ class Cannon{
     //객체지향에서는 움직임을 정의하는 코드가 바로 함수이며, 이 함수는 객체에 소속이 되므로
     //메서드라 불린다 
     move(){
-        this.x += 5;
+        this.x += this.velX;
+        this.y += this.velY;
+
+        //위의 증가분을 이용하여 화면에 보여주기 (랜더링)
+        this.img.style.top=this.y+"px";
+        this.img.style.left=this.x+"px";
     }
 }
