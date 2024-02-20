4232b8a813c80fef9829953459040d24f36cea86
multicampus202402 <multicam202402@gmail.com>
2024-02-20 15:40:19 +0900

    금일수업

--- day8/Ball.js
+++ day8/Ball.js
@@ -2,16 +2,23 @@
 class Ball{
 
     constructor(container, bg, x, y, width, height, velX, velY, text){
-        this.container;
-        this.div;
-        this.bg;
-        this.x;
-        this.y;
-        this.width;
-        this.height;
-        this.velX;
-        this.velY;
-        this.text;//공의 좌표를 출력하기 위한 텍스트
+        this.container=container;
+        this.div=document.createElement("div");
+        this.bg=bg;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.velX=velX;
+        this.velY=velY;
+        this.text=text;//공의 좌표를 출력하기 위한 텍스트
 
+        //style 적용 
+        this.div.style.background=this.bg;
+        
+        this.div.style.position="absolute";
+        this.div.style.top=this.y+"px";
+        this.div.style.left=this.x+"px";
+        
     }
 }
