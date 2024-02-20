04f97be6cf9ba07646034bc4595f906b55a1ed5a
multicampus202402 <multicam202402@gmail.com>
2024-02-20 16:15:02 +0900

    금일수업

--- day8/Ball.js
+++ day8/Ball.js
@@ -16,7 +16,7 @@ class Ball{
         //style 적용 
         this.div.style.background=this.bg;
         this.div.style.borderRadius=50+"%";
-        
+
         this.div.style.position="absolute";
         this.div.style.top=this.y+"px";
         this.div.style.left=this.x+"px";
@@ -26,5 +26,21 @@ class Ball{
 
         //부모 요소에 부착 
         this.container.appendChild(this.div);
+    }
+
+    //공 움직이기 
+    move(){
+        //물리량 변경
+        this.x += this.velX;
+        this.y += this.velY;
+
+        //공의 현재 x+width 값이, 우측 벽의 한계(900)에 도달하면 velX의 부호를 반대로 전환 -1 곱하기
+        //공의 현재 x값이 , 좌측 벽의 한계(0)에 도달하면 velX의 부호를 반대로 전환
+        //공의 현재 y값이, 위쪽 벽의 한계(0)에 도달하면 velY의 부호를 반대로 전환 
+        //공의 현재 y+height값이 , 아래쪽 벽의 한계(900)에 도달하면 .velY부호를 반대로 전환
+
+        //렌더링
+        this.div.style.top=this.y+"px";
+        this.div.style.left=this.x+"px";        
     }
 }
