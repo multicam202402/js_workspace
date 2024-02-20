bfac2365c73ce4bcc2280b5dbb43e3e367f1409b
multicampus202402 <multicam202402@gmail.com>
2024-02-20 15:38:12 +0900

    금일수업

---
+++ day8/Ball.js
@@ -0,0 +1,17 @@
+/*공을 정의한다 */
+class Ball{
+
+    constructor(container, bg, x, y, width, height, velX, velY, text){
+        this.container;
+        this.div;
+        this.bg;
+        this.x;
+        this.y;
+        this.width;
+        this.height;
+        this.velX;
+        this.velY;
+        this.text;//공의 좌표를 출력하기 위한 텍스트
+
+    }
+}
