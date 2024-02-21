6be72f6c3b569ee0a303b83961359ecc675ed97e
multicampus202402 <multicam202402@gmail.com>
2024-02-21 11:30:33 +0900

    금일수업

--- day9/Enemy.js
+++ day9/Enemy.js
@@ -1,14 +1,27 @@
 /*적군을 정의한다*/
 class Enemy{
-    constructor(){
-        this.container;
-        this.img;
-        this.src;
-        this.x;
-        this.y;
-        this.width;
-        this.height;
-        this.velX;
-        this.velY;
+    constructor(container, src, direct, x, y, width, height,velX,velY){
+        this.container=container;
+        this.img=document.createElement("img");
+        this.src=src;
+        this.direct=direct; //양수 1, -1 좌
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.velX=velX;
+        this.velY=velY;
+
+        //style
+        this.img.src=this.src;
+
+        this.img.style.position="absolute";
+        this.img.style.left=this.x+"px";
+        this.img.style.top=this.y+"px";
+
+        this.img.style.width=this.width+"px";
+        this.img.style.height=this.height+"px";
+
+        this.container.appendChild(this.img);
     }
 }
