52c6935f4d3d1b78fbedce7d8df64baf88b07e80
multicampus202402 <multicam202402@gmail.com>
2024-02-21 14:51:45 +0900

    금일수업

--- js/lib.js
+++ js/lib.js
@@ -9,7 +9,7 @@ function dan(n){
 /*----------------------------------------------
 랜덤값 반환하기    
 max 값까지의 랜덤한 값 구하기 단,
-getRandom(5);   0~5 까지
+ex) getRandom(5);   0~5 까지
 ----------------------------------------------*/
 function getRandom(max){
     let n = (max+1)*Math.random(); //0.0xxxxx  1보다 작은 실수가 난수로 반환됨
@@ -29,6 +29,27 @@ function getRandomByRange(min, max){
     return result;
 }
 
+
+/*----------------------------------------------
+두물체간 충돌을 체크하는 함수 
+반환값이 true 이며 충돌이다 
+----------------------------------------------*/
+function collisionCheck(box1, box2){
+    //좌측상단 모서리(북서)1사분면
+    let side1 = ((box1.x+box1.width) >= box2.x) && ((box1.y+box1.height) >= box2.y);
+
+    //우측상단 모서리 (북동) 2사분면
+    let side2 = (box1.x <= (box2.x+box2.width)) && ((box1.y+box1.height) >= box2.y);
+
+    //우측하단 모서리(동남) 3사분면 
+    let side3 = (box1.x <= (box2.x+box2.width)) && (box1.y <=(box2.y+box2.height));
+
+    //좌측하단 모서리(서남) 4사분면
+    let side4 = ( (box1.x+box1.width) >= box2.x ) && (box1.y <=(box2.y+box2.height));
+
+    //모든 사분면이 true 값일때, 전체를 true본다 즉 충돌로 본다    
+    return (side1 && side2 && side3 && side4);
+}
 
 
 
