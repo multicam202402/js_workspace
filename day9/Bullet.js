a1a2b61871be9245a594c650281ee9168f5bb08a
multicampus202402 <multicam202402@gmail.com>
2024-02-21 14:35:35 +0900

    금일수업

--- day9/Bullet.js
+++ day9/Bullet.js
@@ -32,6 +32,24 @@ class Bullet{
         //거푸집으로부터 태어난 인스턴스가 자기 자신을 가리킬수 있는 변수 
         //아래의 코드에서, move() 내에서 인스턴스인 현재 총알이 자기자신을 
         //가리켜야 하므로 this를 사용함
+
+        //현재 총알의 인스턴스와 게임에 존재하는 모든 적군과의 충돌검사 후, 
+        //충돌이면 (총알을 화면에서제거+ 총알을  배열에서제거) 나 죽고
+        //충돌이면 (적군도 화면에서제거+ 적군도  배열에서제거) 너 죽고
+        for(let i=0; i< enemyArray.length;i++){ //적군 수만큼 ...
+
+            let result = collisionCheck(this , enemyArray[i]);//충돌여부를 판단하기
+            if(result){//충돌 감지
+                //나죽고 (화면제거 + 배열에서 제거 )
+                wrapper.removeChild(this.div);
+                bulletArray.splice( bulletArray.indexOf(this) , 1 );
+
+                //(적군)너죽고(화면제거 + 배열에서 제거)
+                wrapper.removeChild(enemyArray[i].div);
+                enemyArray.splice(i , 1);
+            }
+        }
+
         //화면의 한계점을 넘어설땐, 총알을 제거(화면에서제거+ 배열에서제거)
         if(this.x >=1280){
             wrapper.removeChild(this.div);//1.화면에서 제거 
