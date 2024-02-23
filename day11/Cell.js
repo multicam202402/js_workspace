3939dc21e13513c312b5196f40787916caea4eed
multicampus202402 <multicam202402@gmail.com>
2024-02-23 11:59:42 +0900

    금일수업

--- day11/Cell.js
+++ day11/Cell.js
@@ -2,7 +2,7 @@
 달력의 셀을 정의
 */
 class Cell{
-    constructor(container, x, y, width, height, content){
+    constructor(container, x, y, width, height, content, icon){
         this.container=container;
         this.div=document.createElement("div");
         this.x=x;
@@ -10,6 +10,14 @@ class Cell{
         this.width=width;
         this.height=height;
         this.content=content; //셀에 채워질 내용
+        this.img=document.createElement("img");
+        this.img.src=icon; //icon 은 이미지의 경로임
+        this.img.style.widht=30+"px";
+        
+        if(icon.length > 0){ //아이콘을 붙이고 싶은 경우만..
+                                    //아이콘의 이미지 경로 문자열이 0이 아니라면..즉 존재한다면
+            this.div.appendChild(this.img);
+        }
 
         //style 
         this.div.style.border="1px solid gray";
