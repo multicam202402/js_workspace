0449a55e04c36cb71c5ad44d2e1f7b6de00e8f70
multicampus202402 <multicam202402@gmail.com>
2024-02-27 13:37:32 +0900

    금일수업

---
+++ day13/Hero.js
@@ -0,0 +1,28 @@
+/*주인공을 정의한다*/
+class Hero{
+    constructor(container, src, x, y, width, height,velX,velY){
+        this.container=container;
+        this.img=document.createElement("img");
+        this.src=src;
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.velX=velX;
+        this.velY=velY;
+
+        this.img.src=this.src;
+
+        //style
+        this.img.style.position="absolute";
+        this.img.style.left=this.x+"px";
+        this.img.style.top=this.y+"px";
+
+        this.img.style.width=this.width+"px";
+        this.img.style.height=this.height+"px";
+
+        //부착 
+        this.container.appendChild(this.img);
+    }
+
+}
